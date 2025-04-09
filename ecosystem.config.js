8a52f513a86b39b4a0ba8b54ddbf64cce1b0592c
Shiv Singh <shiv@srapsware.com>
2025-04-09 18:38:12 +0530

    Enhance deployment script with improved swap space setup and error handling; switch Strapi script to npm for consistency

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -2,12 +2,13 @@ module.exports = {
   apps: [
     {
       name: 'strapi',
-      script: 'yarn',
+      script: 'npm',
       args: 'start',
       env: {
         NODE_ENV: 'production',
       },
       exp_backoff_restart_delay: 100,
+      max_memory_restart: '500M'
     },
   ],
 };
