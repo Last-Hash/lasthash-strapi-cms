044b8ac9c45e0c2cdea0f82833c13fdfbe20c49a
Shiv Singh <shiv@srapsware.com>
2025-04-11 10:15:44 +0530

    feat: update security middleware configuration and add upload plugin dependency

--- config/plugins.js
+++ config/plugins.js
@@ -1,14 +1,29 @@
 module.exports = ({ env }) => ({
-    email: {
-      config: {
-        provider: 'strapi-provider-email-resend',
-        providerOptions: {
-          apiKey: env('RESEND_API_KEY'), // Required
-        },
-        settings: {
-          defaultFrom: 'contact@lasthash.com',
-          defaultReplyTo: 'contact@lasthash.com',
-        },
-      }
-    },    
-  });
+  email: {
+    config: {
+      provider: 'strapi-provider-email-resend',
+      providerOptions: {
+        apiKey: env('RESEND_API_KEY'), // Required
+      },
+      settings: {
+        defaultFrom: 'contact@lasthash.com',
+        defaultReplyTo: 'contact@lasthash.com',
+      },
+    }
+  },
+  upload: {
+    config: {
+      provider: 'cloudinary',
+      providerOptions: {
+        cloud_name: env('CLOUDINARY_NAME'),
+        api_key: env('CLOUDINARY_KEY'),
+        api_secret: env('CLOUDINARY_SECRET'),
+      },
+      actionOptions: {
+        upload: {},
+        uploadStream: {},
+        delete: {},
+      },
+    },
+  },
+});
